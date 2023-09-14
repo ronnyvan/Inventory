9957b46b5dfb08efe4d5db76b1857f49bd16185a
RONNYVAN <115511513+ronnyvan@users.noreply.github.com>
2023-09-13 19:36:18 -0500

    Create ListNode.java

---
+++ ListNode.java
@@ -0,0 +1,48 @@
+
+public class ListNode
+{
+    private Object value;
+    private ListNode next;
+    
+    //constructor(s)
+    public ListNode(Object val)
+    {
+        value = val;
+        next = null;
+    }
+    
+    public ListNode(Object val, ListNode nectNode)
+    {
+        value = val; 
+        next = nectNode;
+    }
+    
+    public Object getValue()
+    {
+        return value;
+    }
+    
+    public ListNode getNext()
+    {
+        return next;
+    }
+    
+    public boolean hasNext()
+    {
+        return (next != null);
+    }
+    
+    public void setValue(Object newVal)
+    {
+        value = newVal;
+    }
+    
+    public void setNext(ListNode newNext)
+    {
+        next = newNext;
+    }
+    public String toString() {
+        return value.toString(); // Assuming value is not null and has a meaningful toString() method
+    }
+
+}
